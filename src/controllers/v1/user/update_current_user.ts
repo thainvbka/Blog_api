8f37f71ad1a30d214a182094191c8930359efbe8
Nguyen Van Thai <nvthai2904@gmail.com>
2025-07-28 17:51:57 +0700

    update current user

---
+++ src/controllers/v1/user/update_current_user.ts
@@ -0,0 +1,78 @@
+/**
+ * custom modules
+ */
+import { logger } from '@/lib/winston';
+
+/**
+ * types
+ */
+import { Request, Response } from 'express';
+
+/**
+ * models
+ */
+import User from '@/models/user';
+
+const updateCurrentUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const userId = req.userId;
+  const {
+    username,
+    email,
+    password,
+    firstName,
+    lastName,
+    website,
+    facebook,
+    instagram,
+    linkedin,
+    x,
+    youtube,
+  } = req.body;
+
+  try {
+    const user = await User.findById(userId).select('+password -__v').exec();
+    if (!user) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'User not found',
+      });
+      return;
+    }
+
+    if (username) user.username = username;
+    if (email) user.email = email;
+    if (password) user.password = password;
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+
+    if (!user.socialLink) {
+      user.socialLink = {};
+    }
+
+    if (website) user.socialLink.website = website;
+    if (facebook) user.socialLink.facebook = facebook;
+    if (instagram) user.socialLink.instagram = instagram;
+    if (linkedin) user.socialLink.linkedin = linkedin;
+    if (x) user.socialLink.x = x;
+    if (youtube) user.socialLink.youtube = youtube;
+
+    await user.save();
+
+    logger.info('User update successfully', user);
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: error,
+    });
+
+    logger.error('Error while updating current user', error);
+  }
+};
+
+export default updateCurrentUser;
--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -20,6 +20,7 @@ import User from '@/models/user';
  * controllers
  */
 import getCurrentUser from '@/controllers/v1/user/get_current_user';
+import updateCurrentUser from '@/controllers/v1/user/update_current_user';
 
 const router = Router();
 
@@ -28,6 +29,57 @@ router.get(
   authenticate,
   authorize(['admin', 'user']),
   getCurrentUser,
+);
+
+router.put(
+  '/current',
+  authenticate,
+  authorize(['admin', 'user']),
+  body('username')
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage('Username must be less than 20 characters')
+    .custom(async (value) => {
+      const userExists = await User.exists({ username: value });
+
+      if (userExists) {
+        throw new Error('This username is already in use');
+      }
+    }),
+  body('email')
+    .optional()
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50 characters')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const userExists = await User.exists({ username: value });
+
+      if (userExists) {
+        throw new Error('This email is already in use');
+      }
+    }),
+  body('password')
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long'),
+  body('firstName')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('First name must be less than 20 characters'),
+  body('lastName')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('Last name must be less than 20 characters'),
+  body(['website', 'facebook', 'instagram', 'linkedin', 'x', 'youtube'])
+    .optional()
+    .isURL()
+    .withMessage('Invalid URL')
+    .isLength({ max: 100 })
+    .withMessage('URL must be less than 100 characters'),
+  validationError,
+  updateCurrentUser,
 );
 
 export default router;
